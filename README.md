79c8dab1c6e9ee4985bb25a9d3d8fe83fe026212
KappaMustafa <steef136@gmail.com>
2022-08-23 12:54:13 -0500

    added links to readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+# Text Editor!
+* I set up the plugins and the module rules.
+* I also implemented asset caching.
+* I added logic to the database.js for it to store and grab data
 
+
+
+## Deployed App
+* Click [Here](https://text-editor-mustafa-a.herokuapp.com/) to test the app in action!
+
+## GitHub Repo
+* Click [Here](https://github.com/KappaMustafa/text_editor) to visit the repo for this app
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,8 +4,8 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     window.deferredPrompt = event;
-    butInstall.classList.toggle("hidden", false);
     // butInstall.classList.add("hidden", false);
+    butInstall.classList.toggle("hidden", false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
